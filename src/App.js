c0d42431acf7acb26bd75c646884a4b774dc89c9
Potchara Puttawanchai <potchangelo@gmail.com>
2021-01-13 14:57:23 +0700

    Starter pack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <section className="app-section">
+            <div className="app-container">
+                <h3>สักหน่อยมั้ยหละ</h3>
+            </div>
+        </section>
+    );
 }
 
 export default App;
