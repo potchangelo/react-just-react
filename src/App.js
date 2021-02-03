23bc358d0685e3f2e4175a3e5fc443c7c389358d
Potchara Puttawanchai <potchangelo@gmail.com>
2021-02-04 01:00:59 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,151 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-const emptyNote = {
-    content: '', author: ''
-};
-
 function App() {
-    // - States
-    const [note, setNote] = useState(emptyNote);
-    const [editNote, setEditNote] = useState(null);
-    const [allNotes, setAllNotes] = useState([]);
+    const [users, setUsers] = useState([]);
 
-    // - Functions form inputs
-    function onNoteValueChange(event) {
-        const { name, value } = event.target;
-        setNote((prevNote) => {
-            return {
-                ...prevNote,
-                [name]: value
-            };
+    useEffect(() => {
+        fetch('https://reqres.in/api/users?page=2').then(res => {
+            return res.json();
+        }).then(resJson => {
+            setUsers(resJson.data);
         });
-    }
+    }, []);
 
-    function onEditNoteValueChange(event) {
-        const { name, value } = event.target;
-        setEditNote((prevEditNote) => {
-            return {
-                ...prevEditNote,
-                [name]: value
-            };
-        });
-    }
-
-    // - Functions add, edit, delete
-    function onNoteSubmit(event) {
-        event.preventDefault();
-
-        // Add note
-        setAllNotes((prevAllNotes) => {
-            const newNote = { ...note };
-            newNote.id = Date.now().toString();
-            return [newNote, ...prevAllNotes];
-        });
-
-        // Clear form
-        setNote(emptyNote);
-    }
-
-    function onEditNoteSubmit(event) {
-        event.preventDefault();
-
-        // Edit note
-        setAllNotes((prevAllNotes) => {
-            return prevAllNotes.map((note) => {
-                if (note.id !== editNote.id) return note;
-                return editNote;
-            });
-        });
-
-        // Clear form
-        setEditNote(null);
-    }
-
-    function onNoteDelete(noteId) {
-        setAllNotes((prevAllNotes) => {
-            return prevAllNotes.filter(note => note.id !== noteId);
-        });
-    }
-
-    // - Elements
-    const allNotesElements = allNotes.map((theNote) => {
-        return (
-            <div key={theNote.id} className="app-note">
-                <p>{theNote.content}</p>
-                <h5>{theNote.author}</h5>
-                <p>
-                    <a onClick={() => {setEditNote(theNote)}}>Edit</a>
-                    <span> | </span>
-                    <a onClick={() => {onNoteDelete(theNote.id)}}>Delete</a>
-                </p>
-            </div>
-        );
+    const userElements = users.map(user => {
+        return <h3 key={user.id}>{user.email}</h3>;
     });
-
-    let editNoteElement = null;
-    if (!!editNote) {
-        editNoteElement = (
-            <div className="app-edit-note">
-                <form onSubmit={onEditNoteSubmit}>
-                    <p>
-                        <textarea
-                            rows="3"
-                            placeholder="บันทึกความในใจ"
-                            name="content"
-                            value={editNote.content}
-                            onChange={onEditNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            type="text"
-                            placeholder="ลงชื่อ"
-                            name="author"
-                            value={editNote.author}
-                            onChange={onEditNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <button type="submit">แก้ไขซะ</button>
-                    </p>
-                </form>
-            </div>
-        );
-    }
 
     return (
         <section className="app-section">
             <div className="app-container">
-                <h3>สักหน่อยมั้ยหละ</h3>
-                <form onSubmit={onNoteSubmit}>
-                    <p>
-                        <textarea
-                            rows="3"
-                            placeholder="บันทึกความในใจ"
-                            name="content"
-                            value={note.content}
-                            onChange={onNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            type="text"
-                            placeholder="ลงชื่อ"
-                            name="author"
-                            value={note.author}
-                            onChange={onNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <button type="submit">เพิ่มลงไป</button>
-                    </p>
-                </form>
-                <div className="app-notes">
-                    {allNotesElements}
-                </div>
+                <h1>เก็บโค้ดส่วนลดไม่ทัน</h1>
+                {userElements}
             </div>
-            {editNoteElement}
         </section>
     );
 }
