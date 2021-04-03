5b619ca15be2f6de74684ca0a3e56eea3c6ed438
Potchara Puttawanchai <potchangelo@gmail.com>
2021-04-03 19:03:05 +0700

    Code reset

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import { useRef } from 'react';
 import './App.css';
-import SelectTextarea from './SelectTextarea';
-import SendFile from './SendFile';
 
 function App() {
-    const textareaRef = useRef(null);
-    const fileRef = useRef(null);
-
-    function onSendClick() {
-        console.log(fileRef.current.files[0]);
-    }
-
-    function onSelectTextClick() {
-        textareaRef.current.focus();
-        textareaRef.current.select();
-    }
-
     return (
         <section className="app-section">
             <div className="app-container">
-                <SelectTextarea onSelectTextClick={onSelectTextClick} ref={textareaRef} />
-                <SendFile onSendClick={onSendClick} ref={fileRef} />
+                <h2>Farm Dashboard</h2>
+                <div className="box">
+                    <h3>ออเดอร์ต่อวัน</h3>
+                    <ul>
+                        <li>3 กันยายน - 1,582 ออเดอร์</li>
+                        <li>2 กันยายน - 1,649 ออเดอร์</li>
+                        <li>1 กันยายน - 1,755 ออเดอร์</li>
+                    </ul>
+                </div>
+                <div className="box">
+                    <h3>ยอดขายสินค้ายอดนิยม</h3>
+                    <p>หมายเหตุ : เป็นราคาโดยประมาณเท่านั้น</p>
+                    <ol>
+                        <li>แครอท ~ 200,000 บาท</li>
+                        <li>มะเขือเทศ ~ 120,000 บาท</li>
+                        <li>แตงกวา ~ 105,000 บาท</li>
+                    </ol>
+                </div>
+                <div className="box">
+                    <h4>Tips of the day</h4>
+                    <p>บัญชีของคุณยังไม่ได้ยืนยันอีเมลล์ กรุณาตรวจสอบเพื่อความเนียนกริ๊บในการใช้ระบบ</p>
+                </div>
             </div>
         </section>
     );
