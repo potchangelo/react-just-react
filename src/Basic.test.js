8255a337d7b743b4d0f8771dc11173bc211c4dc1
Potchara Puttawanchai <potchangelo@gmail.com>
2021-05-16 16:15:34 +0700

    Update function naming as JS

--- src/Basic.test.js
+++ src/Basic.test.js
@@ -1,4 +1,4 @@
-function rectangle_area(width, height) {
+function rectangleArea(width, height) {
     if (width <= 0 || height <= 0) {
         return 0;
     }
@@ -6,17 +6,17 @@ function rectangle_area(width, height) {
 }
 
 test('Normal rectangle area, width > 0 and height > 0', () => {
-    const rect1 = rectangle_area(8, 6);
+    const rect1 = rectangleArea(8, 6);
     expect(rect1).toBe(48);
 });
 
 test('Zero rectangle area, width <= 0 or height <= 0', () => {
-    const rect1 = rectangle_area(-4, 6);
+    const rect1 = rectangleArea(-4, 6);
     expect(rect1).toBe(0);
 
-    const rect2 = rectangle_area(4, -6);
+    const rect2 = rectangleArea(4, -6);
     expect(rect2).toBe(0);
 
-    const rect3 = rectangle_area(-4, -6);
+    const rect3 = rectangleArea(-4, -6);
     expect(rect3).toBe(0);
 });
