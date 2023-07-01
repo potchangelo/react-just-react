1c753bccf2a3322048cf2147f65f64b0c1e0c89b
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:58:57 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -5,22 +5,22 @@ import PageAbout from './PageAbout';
 import Page404 from './Page404';
 
 function App() {
-    return (
-        <div>
-            <AppHeader />
-            <Switch>
-                <Route path="/" exact>
-                    <PageHome />
-                </Route>
-                <Route path="/about">
-                    <PageAbout />
-                </Route>
-                <Route path="*">
-                    <Page404 />
-                </Route>
-            </Switch>
-        </div>
-    );
+  return (
+    <div>
+      <AppHeader />
+      <Switch>
+        <Route path="/" exact>
+          <PageHome />
+        </Route>
+        <Route path="/about">
+          <PageAbout />
+        </Route>
+        <Route path="*">
+          <Page404 />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/LayoutPage.js
+++ src/LayoutPage.js
@@ -1,13 +1,11 @@
 import './LayoutPage.css';
 
 function LayoutPage(props) {
-    return (
-        <section className="layout-page-section">
-            <div className="layout-page-container">
-                {props.children}
-            </div>
-        </section>
-    );
+  return (
+    <section className="layout-page-section">
+      <div className="layout-page-container">{props.children}</div>
+    </section>
+  );
 }
 
 export default LayoutPage;
