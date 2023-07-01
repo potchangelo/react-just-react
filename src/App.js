a7d6916077f7f64de3559d53afec29eb9ba64bfc
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:04:18 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -3,23 +3,86 @@ import './App.css';
 import Popup from './Popup';
 
 function App() {
-    const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    let popup = null;
-    if (isPopupOpen) {
-        popup = <Popup onPopupClose={() => setIsPopupOpen(false)} />;
-    }
+  let popup = null;
+  if (isPopupOpen) {
+    popup = <Popup onPopupClose={() => setIsPopupOpen(false)} />;
+  }
 
-    return (
-        <section className="app-section">
-            <div className="app-container">
-                <h1>เก็บโค้ดส่วนลดไม่ทัน</h1>
-                <button onClick={() => setIsPopupOpen(true)}>เปิดป๊อปอัพ</button>
-                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
-            </div>
-            {popup}
-        </section>
-    );
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h1>ใครเอ่ย</h1>
+        <button onClick={() => setIsPopupOpen(true)}>เปิดป๊อปอัพ</button>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+        <p>น่ารัก</p>
+      </div>
+      {popup}
+    </section>
+  );
 }
 
 export default App;
