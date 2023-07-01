193b9cb88e971d1c932f448e05e3aaab927ba8c4
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 15:27:20 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -2,93 +2,92 @@ import { useState } from 'react';
 import './App.css';
 
 const emptyNote = {
-    content: '', author: ''
+  content: '',
+  author: '',
 };
 
 function App() {
-    // - States
-    const [note, setNote] = useState(emptyNote);
-    const [allNotes, setAllNotes] = useState([]);
+  // - States
+  const [note, setNote] = useState(emptyNote);
+  const [allNotes, setAllNotes] = useState([]);
 
-    // - Functions
-    function onNoteValueChange(event) {
-        const { name, value } = event.target;
-        setNote((prevNote) => {
-            return {
-                ...prevNote,
-                [name]: value
-            };
-        });
-    }
+  // - Functions
+  function onNoteValueChange(event) {
+    const { name, value } = event.target;
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
 
-    function onNoteSubmit(event) {
-        event.preventDefault();
+  function onNoteSubmit(event) {
+    event.preventDefault();
 
-        // Add note
-        setAllNotes((prevAllNotes) => {
-            const newNote = { ...note };
-            newNote.id = Date.now().toString();
-            return [newNote, ...prevAllNotes];
-        });
-
-        // Clear form
-        setNote(emptyNote);
-    }
-
-    function onNoteDelete(noteId) {
-        setAllNotes((prevAllNotes) => {
-            return prevAllNotes.filter(theNote => theNote.id !== noteId);
-        });
-    }
-
-    // - Elements
-    const allNotesElements = allNotes.map((theNote) => {
-        return (
-            <div key={theNote.id} className="app-note">
-                <p>{theNote.content}</p>
-                <h5>{theNote.author}</h5>
-                <p>
-                    <a>Edit</a>
-                    <span> | </span>
-                    <a onClick={() => {onNoteDelete(theNote.id)}}>Delete</a>
-                </p>
-            </div>
-        );
+    // Add note
+    setAllNotes(prevAllNotes => {
+      const newNote = { ...note };
+      newNote.id = Date.now().toString();
+      return [newNote, ...prevAllNotes];
     });
 
+    // Clear form
+    setNote(emptyNote);
+  }
+
+  function onNoteDelete(noteId) {
+    setAllNotes(prevAllNotes => {
+      return prevAllNotes.filter(theNote => theNote.id !== noteId);
+    });
+  }
+
+  // - Elements
+  const allNotesElements = allNotes.map(theNote => {
     return (
-        <section className="app-section">
-            <div className="app-container">
-                <h3>สักหน่อยมั้ยหละ</h3>
-                <form onSubmit={onNoteSubmit}>
-                    <p>
-                        <textarea
-                            rows="3"
-                            placeholder="บันทึกความในใจ"
-                            name="content"
-                            value={note.content}
-                            onChange={onNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <input
-                            type="text"
-                            placeholder="ลงชื่อ"
-                            name="author"
-                            value={note.author}
-                            onChange={onNoteValueChange}
-                        />
-                    </p>
-                    <p>
-                        <button type="submit">เพิ่มไปเลยสิค่ะที่รัก</button>
-                    </p>
-                </form>
-                <div className="app-notes">
-                    {allNotesElements}
-                </div>
-            </div>
-        </section>
+      <div key={theNote.id} className="app-note">
+        <p>{theNote.content}</p>
+        <h5>{theNote.author}</h5>
+        <p>
+          <a>Edit</a>
+          <span> | </span>
+          <a
+            onClick={() => {
+              onNoteDelete(theNote.id);
+            }}
+          >
+            Delete
+          </a>
+        </p>
+      </div>
     );
+  });
+
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h3>สักหน่อยมั้ยหละ</h3>
+        <form onSubmit={onNoteSubmit}>
+          <p>
+            <textarea
+              rows="3"
+              placeholder="บันทึกความในใจ"
+              name="content"
+              value={note.content}
+              onChange={onNoteValueChange}
+            />
+          </p>
+          <p>
+            <input type="text" placeholder="ลงชื่อ" name="author" value={note.author} onChange={onNoteValueChange} />
+          </p>
+          <p>
+            <button type="submit">เพิ่มไปเลยสิค่ะที่รัก</button>
+          </p>
+        </form>
+        <div className="app-notes">{allNotesElements}</div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
