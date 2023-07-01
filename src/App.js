0b816e76bab2d3306c97ce8d13f9dcbe98328f0c
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:17:18 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import LoginArea from './LoginArea';
 const AuthContext = React.createContext();
 
 function App() {
-    const [auth, setAuth] = useState(null);
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            <section className="app-section">
-                <div className="app-container">
-                    <LoginArea />
-                </div>
-            </section>
-        </AuthContext.Provider>
-    );
+  const [auth, setAuth] = useState(null);
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      <section className="app-section">
+        <div className="app-container">
+          <LoginArea />
+        </div>
+      </section>
+    </AuthContext.Provider>
+  );
 }
 
 export { AuthContext };
--- src/LoginBox.js
+++ src/LoginBox.js
@@ -1,39 +1,47 @@
-import { useContext } from "react";
-import { AuthContext } from "./App";
+import { useContext } from 'react';
+import { AuthContext } from './App';
 
 const fakeUser = { username: 'nice789', fullname: 'เต้า หมิงซื่อ' };
 
 function LoginBox() {
-    const { auth, setAuth } = useContext(AuthContext);
+  const { auth, setAuth } = useContext(AuthContext);
 
-    function loginSubmit(event) {
-        event.preventDefault();
-        setAuth(fakeUser);
-    }
+  function loginSubmit(event) {
+    event.preventDefault();
+    setAuth(fakeUser);
+  }
 
-    function logoutSubmit() {
-        setAuth(null);
-    }
+  function logoutSubmit() {
+    setAuth(null);
+  }
 
-    if (!!auth) {
-        return (
-            <div>
-                <h3>เข้าสู่ระบบแล้วจ้า</h3>
-                <p>Auth username = {auth.username}</p>
-                <p>Auth fullname = {auth.fullname}</p>
-                <p><button onClick={logoutSubmit}>Log out</button></p>
-            </div>
-        );
-    }
-
+  if (!!auth) {
     return (
-        <form onSubmit={loginSubmit}>
-            <h3>โปรดเข้าสู่ระบบ</h3>
-            <p><input type="text" placeholder="Username" /></p>
-            <p><input type="password" placeholder="Password" /></p>
-            <p><button type="submit">Log in</button></p>
-        </form>
+      <div>
+        <h3>เข้าสู่ระบบแล้วจ้า</h3>
+        <p>Auth username = {auth.username}</p>
+        <p>Auth fullname = {auth.fullname}</p>
+        <p>
+          <button onClick={logoutSubmit}>Log out</button>
+        </p>
+      </div>
     );
+  }
+
+  return (
+    <form onSubmit={loginSubmit}>
+      <h3>โปรดเข้าสู่ระบบ</h3>
+      <p>
+        <input type="text" placeholder="Username" />
+      </p>
+      <p>
+        <input type="password" placeholder="Password" />
+      </p>
+      <p>
+        <button type="submit">Log in</button>
+      </p>
+    </form>
+  );
 }
 
 export default LoginBox;
