f320d40447e08dc183eccc60a9847cd21c85fe7d
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 13:08:38 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import BoxInput from './BoxInput';
 import BoxOutput from './BoxOutput';
 
 function App() {
-    const [bmi, setBmi] = useState(null);
+  const [bmi, setBmi] = useState(null);
 
-    return (
-        <AppSection>
-            <AppContainer>
-                <h2>เว็บแอพคำนวณ BMI</h2>
-                <BoxInput onCalculateBmi={setBmi} />
-                <BoxOutput bmi={bmi} />
-            </AppContainer>
-        </AppSection>
-    );
+  return (
+    <AppSection>
+      <AppContainer>
+        <h2>เว็บแอพคำนวณ BMI</h2>
+        <BoxInput onCalculateBmi={setBmi} />
+        <BoxOutput bmi={bmi} />
+      </AppContainer>
+    </AppSection>
+  );
 }
 
 const AppSection = styled.section`
-    padding: 20px;
+  padding: 20px;
 `;
 
 const AppContainer = styled.section`
-    max-width: 900px;
-    margin-left: auto;
-    margin-right: auto;
+  max-width: 900px;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 export default App;
--- src/BoxOutput.js
+++ src/BoxOutput.js
@@ -1,35 +1,33 @@
 import styled from 'styled-components';
 
 function BoxOutput(props) {
-    const { bmi } = props;
+  const { bmi } = props;
 
-    if (!bmi) {
-        return <OutputDiv>---</OutputDiv>;
-    }
+  if (!bmi) {
+    return <OutputDiv>---</OutputDiv>;
+  }
 
-    let result = '';
-    if (bmi < 18.5) {
-        result = 'ผอม';
-    }
-    else if (bmi >= 18.5 && bmi <=25) {
-        result = 'ปกติ';
-    }
-    else {
-        result = 'อ้วน';
-    }
+  let result = '';
+  if (bmi < 18.5) {
+    result = 'ผอม';
+  } else if (bmi >= 18.5 && bmi <= 25) {
+    result = 'ปกติ';
+  } else {
+    result = 'อ้วน';
+  }
 
-    return (
-        <OutputDiv>
-            <h3>BMI = {bmi.toFixed(2)}</h3>
-            <h2>ผลลัพธ์ = {result}</h2>
-        </OutputDiv>
-    );
+  return (
+    <OutputDiv>
+      <h3>BMI = {bmi.toFixed(2)}</h3>
+      <h2>ผลลัพธ์ = {result}</h2>
+    </OutputDiv>
+  );
 }
 
 const OutputDiv = styled.div`
-    border: 8px double skyblue;
-    text-align: center;
-    padding: 16px;
+  border: 8px double skyblue;
+  text-align: center;
+  padding: 16px;
 `;
 
 export default BoxOutput;
