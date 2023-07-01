905cd0559aecc8a441d0f50ab1b55e9a3fe40929
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:00:39 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        fetch('https://reqres.in/api/users?page=2').then(res => {
-            return res.json();
-        }).then(resJson => {
-            setUsers(resJson.data);
-        });
-    }, []);
+  useEffect(() => {
+    fetch('https://reqres.in/api/users?page=2')
+      .then(res => {
+        return res.json();
+      })
+      .then(resJson => {
+        setUsers(resJson.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
-    const userElements = users.map(user => {
-        return <h3 key={user.id}>{user.email}</h3>;
-    });
+  const userElements = users.map(user => {
+    return <h3 key={user.id}>{user.email}</h3>;
+  });
 
-    return (
-        <section className="app-section">
-            <div className="app-container">
-                <h1>เก็บโค้ดส่วนลดไม่ทัน</h1>
-                {userElements}
-            </div>
-        </section>
-    );
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h1>ใครเอ่ย</h1>
+        {userElements}
+      </div>
+    </section>
+  );
 }
 
 export default App;
