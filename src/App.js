22cc27e822ea8beeff14b4571f05437d0c128dfc
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:18:58 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ import LoginArea from './LoginArea';
 const AuthContext = React.createContext();
 
 function reducer(state, action) {
-    if (action.type === 'login') {
-        const { username, fullname } = action.payload;
-        return { username, fullname };
-    }
-    if (action.type === 'logout') {
-        return null;
-    }
-    return state;
+  if (action.type === 'login') {
+    const { username, fullname } = action.payload;
+    return { username, fullname };
+  }
+  if (action.type === 'logout') {
+    return null;
+  }
+  return state;
 }
 
 function App() {
-    const [authState, authDispatch] = useReducer(reducer, null);
-    return (
-        <AuthContext.Provider value={{ authState, authDispatch }}>
-            <section className="app-section">
-                <div className="app-container">
-                    <LoginArea />
-                </div>
-            </section>
-        </AuthContext.Provider>
-    );
+  const [authState, authDispatch] = useReducer(reducer, null);
+  return (
+    <AuthContext.Provider value={{ authState, authDispatch }}>
+      <section className="app-section">
+        <div className="app-container">
+          <LoginArea />
+        </div>
+      </section>
+    </AuthContext.Provider>
+  );
 }
 
 export { AuthContext };
--- src/LoginBox.js
+++ src/LoginBox.js
@@ -1,39 +1,47 @@
-import { useContext } from "react";
-import { AuthContext } from "./App";
+import { useContext } from 'react';
+import { AuthContext } from './App';
 
 const fakeUser = { username: 'nice789', fullname: 'เต้า หมิงซื่อ' };
 
 function LoginBox() {
-    const { authState, authDispatch } = useContext(AuthContext);
+  const { authState, authDispatch } = useContext(AuthContext);
 
-    function loginSubmit(event) {
-        event.preventDefault();
-        authDispatch({ type: 'login', payload: fakeUser });
-    }
+  function loginSubmit(event) {
+    event.preventDefault();
+    authDispatch({ type: 'login', payload: fakeUser });
+  }
 
-    function logoutSubmit() {
-        authDispatch({ type: 'logout' });
-    }
+  function logoutSubmit() {
+    authDispatch({ type: 'logout' });
+  }
 
-    if (!!authState) {
-        return (
-            <div>
-                <h3>เข้าสู่ระบบแล้วจ้า</h3>
-                <p>Auth username = {authState.username}</p>
-                <p>Auth fullname = {authState.fullname}</p>
-                <p><button onClick={logoutSubmit}>Log out</button></p>
-            </div>
-        );
-    }
-
+  if (!!authState) {
     return (
-        <form onSubmit={loginSubmit}>
-            <h3>โปรดเข้าสู่ระบบ</h3>
-            <p><input type="text" placeholder="Username" /></p>
-            <p><input type="password" placeholder="Password" /></p>
-            <p><button type="submit">Log in</button></p>
-        </form>
+      <div>
+        <h3>เข้าสู่ระบบแล้วจ้า</h3>
+        <p>Auth username = {authState.username}</p>
+        <p>Auth fullname = {authState.fullname}</p>
+        <p>
+          <button onClick={logoutSubmit}>Log out</button>
+        </p>
+      </div>
     );
+  }
+
+  return (
+    <form onSubmit={loginSubmit}>
+      <h3>โปรดเข้าสู่ระบบ</h3>
+      <p>
+        <input type="text" placeholder="Username" />
+      </p>
+      <p>
+        <input type="password" placeholder="Password" />
+      </p>
+      <p>
+        <button type="submit">Log in</button>
+      </p>
+    </form>
+  );
 }
 
 export default LoginBox;
