6c1b7cba62c1f5f73836f12bf702d93deec9c6d1
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 16:20:45 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -2,36 +2,36 @@ import { useRef } from 'react';
 import './App.css';
 
 function App() {
-    const textareaRef = useRef(null);
-    const fileRef = useRef(null);
+  const textareaRef = useRef(null);
+  const fileRef = useRef(null);
 
-    function onSendClick() {
-        console.log(fileRef.current.files[0]);
-    }
+  function onSendClick() {
+    console.log(fileRef.current.files[0]);
+  }
 
-    function onSelectTextClick() {
-        textareaRef.current.focus();
-        textareaRef.current.select();
-    }
+  function onSelectTextClick() {
+    textareaRef.current.focus();
+    textareaRef.current.select();
+  }
 
-    return (
-        <section className="app-section">
-            <div className="app-container">
-                <p>
-                    <textarea ref={textareaRef}></textarea>
-                </p>
-                <p>
-                    <button onClick={onSelectTextClick}>เลือกข้อความทั้งหมด</button>
-                </p>
-                <p>
-                    <input type="file" ref={fileRef} />
-                </p>
-                <p>
-                    <button onClick={onSendClick}>ส่งไฟล์</button>
-                </p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <p>
+          <textarea ref={textareaRef}></textarea>
+        </p>
+        <p>
+          <button onClick={onSelectTextClick}>เลือกข้อความทั้งหมด</button>
+        </p>
+        <p>
+          <input type="file" ref={fileRef} />
+        </p>
+        <p>
+          <button onClick={onSendClick}>ส่งไฟล์</button>
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default App;
