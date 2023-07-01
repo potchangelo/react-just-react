6ca9e79ee83c3e51639dd3e4aa79b5a088d7ac7d
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 13:19:52 +0700

    Prettier format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 function App() {
-    return (
-        <section className="app-section">
-            <div className="app-container">
-                <h3>สักหน่อยมั้ยหละ</h3>
-            </div>
-        </section>
-    );
+  return (
+    <section className="app-section">
+      <div className="app-container">
+        <h3>สักหน่อยมั้ยหละ</h3>
+      </div>
+    </section>
+  );
 }
 
 export default App;
